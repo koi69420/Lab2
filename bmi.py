cb04cc968b59894376d7e7800c20f2035edb2503
Koi69420 <leowjianzhi@gmail.com>
2024-04-22 15:56:06 +0800

    bmi lol

---
+++ bmi.py
@@ -0,0 +1,18 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi=(weight)/(height)*(height)
+    
+    print(...)
+
+    if bmi<18.5:
+        print("your wang small coz u don't eat lmao")
+    
+    if bmi<=25.0:
+        print("eh not bad wang not bad")    
+    
+    else: 
+        print("welcome to big wang gang")
+    
+calculate_bmi(weight=60, height=1.8)
