07fb3eee3e3bd0fa10e5a2085af0a1d7fd63c3d2
Koi69420 <leowjianzhi@gmail.com>
2024-04-29 15:04:00 +0800

    weight classification

--- bmi.py
+++ bmi.py
@@ -2,17 +2,19 @@ def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
 
-    bmi=(weight)/(height)*(height)
-    
-    print(...)
+    bmi = weight / (height * height)
 
-    if bmi<18.5:
-        print("your wang small coz u don't eat lmao")
-    
-    if bmi<=25.0:
-        print("eh not bad wang not bad")    
-    
-    else: 
-        print("welcome to big wang gang")
-    
-calculate_bmi(weight=60, height=1.8)
+    print("BMI = {:.2f}".format(bmi))
+
+    if bmi < 18.5:
+        print("Underweight - your wang small coz u don't eat lmao")
+        return -1
+    elif bmi <= 25.0:
+        print("Normal weight - eh not bad wang not bad")
+        return 0
+    else:
+        print("Overweight - welcome to big wang gang")
+        return 1
+
+weight_classification = calculate_bmi(weight=60, height=1.8)
+print("Weight Classification:", weight_classification)
